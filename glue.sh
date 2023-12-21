e81aa19aecc72a7cdc82f932dc79961042c85e01
Luke Boswell <lukewilliamboswell@gmail.com>
2023-12-21 16:29:12 +1100

    (re)-generate glue for Roc types

--- glue.sh
+++ glue.sh
@@ -1,1 +1,7 @@
-roc glue ../roc/crates/glue/src/RustGlue.roc platform/glue/ platform/main.roc
+
+# Note the game developer will need to have roc repository cloned on their 
+# system to (re)generate the glue types.
+#
+# For users of the Roc platform this wont be needed as the game developer should
+# package that into a URL with the artefacts compiled for each supported distribution/architecture
+roc glue ../roc/crates/glue/src/RustGlue.roc generated_glue platform/main.roc
