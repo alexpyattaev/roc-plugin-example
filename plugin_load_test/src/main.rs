54e7e2fd163703ddc034eef8c2250c988ea93467
Alex Pyattaev <alex.pyattaev@gmail.com>
2023-12-23 22:17:43 +0200

    figuring out the reason for segfaults

--- plugin_load_test/src/main.rs
+++ plugin_load_test/src/main.rs
@@ -4,6 +4,12 @@ use roc_app::*;
 //use roc_ffi::force_export;
 use roc_std::RocStr;
 
+
+pub unsafe fn any_as_u8_slice<T: Sized>(p: &T) -> &[u8] {
+    ::core::slice::from_raw_parts((p as *const T) as *const u8, ::core::mem::size_of::<T>())
+}
+
+
 const ROC_MAIN_MAGIC:&[u8]=b"roc__mainForHost_1_exposed_generic";
 fn load_plugin(name: &str) {
     unsafe {
@@ -22,13 +28,18 @@ fn load_plugin(name: &str) {
 
         println!("Function extracted");
         // can I now simply call roc?? is it that easy
-        let mut ret = core::mem::MaybeUninit::<EngineCallins>::uninit();
+        let mut ret = core::mem::MaybeUninit::<EngineCallins>::zeroed();                
         //let arg = RocStr::from("BACKGROUND_COLOR");
         println!("Time to call!");
         func(ret.as_mut_ptr(), 0);
+        
         println!("Call successful!");
         let ret = ret.assume_init();
-        dbg!(ret);
+        //let rv = ret.reset.force_thunk(42);
+        let slice = any_as_u8_slice(&ret);
+        println!("The memory slice below should have function pointers in it...");
+        dbg!(slice);
+        //dbg!(&ret);
     }
 }
 
@@ -37,9 +48,8 @@ fn main() {
 
     let mut args = std::env::args().skip(1);
     let dllname = args
-        .next()
-        .expect("Expeceted absolute path to library to load");
-
+        .next().unwrap_or("./plugin_logic.so".to_owned());
+        //.expect("Expeceted absolute path to library to load");
     println!("Will load {}", dllname);
 
     load_plugin(&dllname);
