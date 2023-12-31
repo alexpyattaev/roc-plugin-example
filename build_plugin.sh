b47d31c1b128636432f78a7d8e8e823e1e59a584
Alex Pyattaev <alex.pyattaev@gmail.com>
2023-12-31 22:40:00 +0200

    plugin wrapper that links correctly

--- build_plugin.sh
+++ build_plugin.sh
@@ -1,6 +1,12 @@
 # generates our dynamic library plugin
 
+
 roc check plugin_logic.roc  || ( echo "Please fix your code!" && exit 1 ) # prevents compiler from crashing when code is really bad 
+
 # build unlinked version of the library 
 roc build --no-link plugin_logic.roc
+# use ar to make an archive that rust linker can eat...
+ar rc platform_wrapper/libapp.a plugin_logic.o
+
+# build shared library version as well since we might want it eventually
 roc build --lib plugin_logic.roc
--- platform_wrapper/build.rs
+++ platform_wrapper/build.rs
@@ -1,9 +1,11 @@
 fn main() {
+    let src_dir = std::env::var("CARGO_MANIFEST_DIR").unwrap();
     #[cfg(not(windows))]
-    println!("cargo:rustc-link-lib=dylib=app");
+    println!("cargo:rustc-link-lib=static=app");
 
     #[cfg(windows)]
-    println!("cargo:rustc-link-lib=dylib=libapp");
+    println!("cargo:rustc-link-lib=static=libapp");
 
-    println!("cargo:rustc-link-search=.");
+    println!("cargo:rustc-link-search={}",src_dir);
+//    println!("cargo:rustc-link-search=all=.");
 }
